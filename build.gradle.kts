7c4a4396b8ded11c3f882430ffa9dca1d7dc2861
MrPowerGamerBR <git@mrpowergamerbr.com>
2020-12-30 09:57:46 -0300

    Update Gradle and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.4.10"
-    kotlin("plugin.serialization") version "1.4.10"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.serialization") version "1.4.21"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "net.perfectdreams.perfectpayments"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -22,34 +22,34 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("ch.qos.logback:logback-classic:1.3.0-alpha5")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.12")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0-RC2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-hocon:1.0.0-RC2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-hocon:1.0.1")
 
     // Database
-    implementation("org.postgresql:postgresql:42.2.14")
+    implementation("org.postgresql:postgresql:42.2.18")
     implementation("com.zaxxer:HikariCP:3.4.5")
-    implementation("org.jetbrains.exposed:exposed-core:0.27.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.27.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.27.1")
+    implementation("org.jetbrains.exposed:exposed-core:0.28.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.28.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.28.1")
 
-    implementation("com.github.ben-manes.caffeine:caffeine:2.8.5")
-    implementation("com.typesafe:config:1.4.0")
-    implementation("org.yaml:snakeyaml:1.26")
+    implementation("com.github.ben-manes.caffeine:caffeine:2.8.8")
+    implementation("com.typesafe:config:1.4.1")
+    implementation("org.yaml:snakeyaml:1.27")
     implementation("org.jsoup:jsoup:1.13.1")
     implementation("com.stripe:stripe-java:20.3.0")
     implementation("com.paypal.sdk:checkout-sdk:1.0.2")
-    implementation("club.minnced:discord-webhooks:0.4.1")
+    implementation("club.minnced:discord-webhooks:0.5.4")
 
-    implementation("io.github.microutils:kotlin-logging:1.8.3")
-    implementation("io.ktor:ktor-client-core:1.4.0")
-    // Has issues with PayPal API for some reason
-    // implementation("io.ktor:ktor-client-cio:1.4.0")
-    implementation("io.ktor:ktor-client-apache:1.4.0")
-    implementation("io.ktor:ktor-server-core:1.4.0")
-    implementation("io.ktor:ktor-server-netty:1.4.0")
-    implementation("io.ktor:ktor-websockets:1.4.0")
+    implementation("io.github.microutils:kotlin-logging:2.0.4")
+
+    // Ktor
+    implementation("io.ktor:ktor-client-core:1.4.3")
+    // We use "Apache" because "CIO", for some reason, has issues with PayPal's API
+    implementation("io.ktor:ktor-client-apache:1.4.3")
+    implementation("io.ktor:ktor-server-core:1.4.3")
+    implementation("io.ktor:ktor-server-netty:1.4.3")
 }
 
 tasks {
