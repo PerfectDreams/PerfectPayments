c76faba423b81da696b016e1196655d5f1228605
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-04-07 16:34:51 -0300

    aaa NOW it should work (second try)

--- frontend/src/jsMain/kotlin/net/perfectdreams/perfectpayments/frontend/components/SelectUserFacingPaymentMethod.kt
+++ frontend/src/jsMain/kotlin/net/perfectdreams/perfectpayments/frontend/components/SelectUserFacingPaymentMethod.kt
@@ -22,11 +22,12 @@ fun SelectUserFacingPaymentMethod(
     Div({ id("wrapper") }) {
         Div({ id("payment-methods") }) {
             Div({ id("payment-method-list") }) {
-                val availableSelections = selections.filterNot { selections is UserFacingPaymentMethod && selections.gateway !in m.availableGateways!! }
+                val availableSelections = selections.filter { selections is UserFacingPaymentMethod && selections.gateway in m.availableGateways!! }
+
                 for ((index, userFacingPaymentSelection) in availableSelections.withIndex()) {
                     UserFacingPaymentButton(m, i18nContext, userFacingPaymentSelection)
 
-                    val isLast = index == selections.size - 1
+                    val isLast = index == availableSelections.size - 1
 
                     if (!isLast) {
                         // Needs to be in a div to not require width: 100%
